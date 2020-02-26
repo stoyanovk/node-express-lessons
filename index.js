574b25ec15ea30111bc9fda195888b53b15dea16
stoyanovk <konstantin@iMac-Konstantin.local>
2020-02-26 12:06:53 +0200

    change order

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use("/add", addRoute);
 app.use("/card", cardRoute);
 app.use("/order", orderRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 async function start() {
   try {
--- routes/order.js
+++ routes/order.js
@@ -3,14 +3,22 @@ const Order = require("../models/Order");
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const orders = await Order.find({ "user.userId": req.user._id });
+  const orders = await Order.find({ "user.userId": req.user._id }).populate(
+    "items.userId"
+  );
+  const resultCourses = orders.map(o => {
+    const course = o._doc.courses.map(c => ({
+      ...c._doc,
+      totalPrice: c._doc.course.price * c._doc.count
+    }));
 
-
-  const resultOrder = orders.map(o => {
-    console.log(o);
+    return course;
   });
+  console.dir(resultCourses);
+
   res.render("order", {
-    title: "Order"
+    title: "Order",
+    resultCourses
   });
 });
 
@@ -20,6 +28,7 @@ router.post("/", async (req, res) => {
   const resultCourses = courses.items.map(item => {
     return { count: item.count, course: item.courseId._doc };
   });
+  console.log(resultCourses);
 
   const order = new Order({
     courses: resultCourses,
