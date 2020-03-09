59b1fc09ec42217769ac96f9c5f3462ba9e0f50c
Konstantin Stoianov <stoyanov.k1992@gmail.com>
2020-03-09 21:26:02 +0200

    add config files

--- mails/reset.js
+++ mails/reset.js
@@ -1,9 +1,11 @@
+const config = require('../settings')
+
 module.exports = function(email,link) {
   return {
     to: email,
     from: "TestNodeProject@mail.com",
     subject: "Reset Password",
     html: `<h1>Reset Pass</h1>
-           <p>For reset pass, go to <a href="http://localhost:3002/auth/reset/password/${link}">link</a></p>`
+           <p>For reset pass, go to <a href="${config.baseUrl}/auth/reset/password/${link}">link</a></p>`
   };
 };
---
+++ settings/keys.prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  url: process.env.url,
+  secretWord: process.env.url,
+  mailKey: process.env.url,
+  baseUrl: process.env.url
+};
